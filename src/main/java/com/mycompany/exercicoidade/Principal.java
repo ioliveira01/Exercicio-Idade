6a8754f4b12932cc0c2527d281fd54d6401f9162
User <User@DESKTOP-MEIFHN3>
2025-09-16 10:35:19 -0300

    Merge origin/main into main

--- src/main/java/com/mycompany/exercicoidade/Principal.java
+++ src/main/java/com/mycompany/exercicoidade/Principal.java
@@ -10,16 +10,14 @@ public class Principal {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Solicita dados
         System.out.print("Digite o ano de nascimento: ");
-        int anoNascimento = scanner.nextInt();
+            int anoNascimento = scanner.nextInt();
 
         System.out.print("Digite o ano atual: ");
-        int anoAtual = scanner.nextInt();
+            int anoAtual = scanner.nextInt();
 
-        // Verifica se o ano de nascimento é válido
-        if (anoNascimento < anoAtual) {
-            int idade = anoAtual - anoNascimento;
+            if (anoNascimento < anoAtual) {
+                int idade = anoAtual - anoNascimento;
             System.out.println("A idade da pessoa é: " + idade + " anos.");
         } else {
             System.out.println("Ano de nascimento inválido! Deve ser menor que o ano atual.");
